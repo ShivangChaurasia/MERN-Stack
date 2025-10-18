fdf7ef2f70660ca1d412fc591489612d8a7b3514
ShivangChaurasia <shiva17ng@gmail.com>
2025-10-19 00:40:29 +0530

    Js Commit

---
+++ JavaScript/JS.js
@@ -0,0 +1,10 @@
+console.log("Hello, World!");
+function add(a, b) {
+    return a + b;
+}
+
+console.log("Sum of 2 and 3 is:", add(2, 3));
+function multiply(a, b) {
+    return a * b;
+}
+console.log("Product of 2 and 3 is:", multiply(2, 3));
