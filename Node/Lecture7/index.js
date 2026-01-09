03328c8c3d36471d73395ffe45240d924b2fd2a6
ShivangChaurasia <shiva17ng@gmail.com>
2026-01-09 13:08:59 +0530

    completed Middleware

--- Node/Lecture7/index.js
+++ Node/Lecture7/index.js
@@ -1,13 +1,57 @@
+const { log } = require('console');
 const express = require('express');
+const fs = require('fs');
 const app = express();
+app.use(express.json());
 
 
-app.get('/',(req,res)=>{
-    return res.status(200).json({message: "This is get Command"})
+app.get('/',(req,res,next)=>{
+    try{
+        return res.status(200).json({message: "This is get Command"})
+    }catch(err){
+        next(err)
+    }
 })
 
+function logDetails(req,res,next){
+    let data = new Date().toLocaleString() + " "+ req.method + "\n";
+    fs.appendFile('./logFile.txt', data, (err)=>{
+        if(err) throw err;
+        next();
+    })
 
+}
+
+app.post('/', logDetails, (req,res)=>{
+    res.send(req.body)
+})
+
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).send({message:"Something went Wrong"})
+})
+
+app.use((req,res,next)=>{
+    res.status(404).send({message: "Route not found at", reqedUrl: req.originalUrl})
+})
 
 app.listen(3000,()=>{
     console.log("Server Started!!");
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
