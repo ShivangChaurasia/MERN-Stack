e99831cb05653b5b4aee92749c9ead5e8eadcb1e
ShivangChaurasia <shiva17ng@gmail.com>
2026-01-11 03:27:38 +0530

    blog.js started

---
+++ Node/Lecture9 - Blog App/blog.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+
+
+async function connectDB(){
+    try{
+        await mongoose.connect("mongodb://localhost:27017/local/blogs")
+        console.log("DB Connected Successfully")
+    }catch(err){
+        console.log(err)
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.use((err,req,res,next)=>{
+    console.log(err)
+    res.status(500).send('Internal Server Error')
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
