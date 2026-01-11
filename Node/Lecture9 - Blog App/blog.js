3f89ca92593610c5f384562cde03e044e326c0bd
ShivangChaurasia <shiva17ng@gmail.com>
2026-01-11 22:55:19 +0530

    Learned to get connected with mongoDB

--- Node/Lecture9 - Blog App/blog.js
+++ Node/Lecture9 - Blog App/blog.js
@@ -1,3 +1,17 @@
+// mongoose: it is a ODM (Object Data Modeling) library for MongoDB and Node.js.
+// it is used to define the schema of the database.
+
+// 127.0.0.1:27017/
+// local: it is the name of the database.
+// blogs: it is the name of the collection.
+
+// schema: it is the blueprint of the database.
+// it defines the structure of the documents in the collection.
+
+// model: it is the constructor function that is used to create documents in the collection.
+// it is created using the schema.
+
+
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express();
@@ -5,7 +19,7 @@ const app = express();
 
 async function connectDB(){
     try{
-        await mongoose.connect("mongodb://localhost:27017/local/blogs")
+        await mongoose.connect("mongodb://localhost:27017/blogs")
         console.log("DB Connected Successfully")
     }catch(err){
         console.log(err)
@@ -15,10 +29,39 @@ async function connectDB(){
 const user = new mongoose.Schema({
     firstname :  String,
     lastname : String,
-    email: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
     password: String
 })
 
+const userModel =  mongoose.model("User", user)
+
+async function createUser(){
+    try{
+        let newUser = await userModel.create({
+            firstname : "Shivang",
+            lastname: "chaurasia",
+            email: "shivangchaurasia@gmail.com",
+            password: "123456"
+        })
+        // let newUser = new userModel({
+        //     firstname : "Shivang 1",
+        //     lastname: "chaurasia1",
+        //     email: "shivangchaurasia17@gmail.com1",
+        //     password: "1234561"
+        // })
+        // await newUser.save()
+        // const user = await userModel.findOneAndDelete({email: "shivangchaurasia17@gmail.com"})
+        console.log(newUser)
+    }catch(err){
+        console.log(err)
+    }
+
+
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -38,4 +81,5 @@ app.use((err,req,res,next)=>{
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
     connectDB()
+    createUser()
 })
